a17794ebec8ee6587f6371848d29f042ba3ef9fa
Santy-G21 <Santi_G2003@hotmail.com>
2024-10-23 11:37:55 -0300

    Add files via upload

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,11 +1,17 @@
 package juego;
 
+
 import entorno.Entorno;
 import entorno.InterfaceJuego;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Juego extends InterfaceJuego {
     private Entorno entorno;
     private Prota prota;
+    private ArrayList<Tortuga> tortugas;
+    private Random random;
 
     public Juego() {
         // Inicializa el objeto entorno
@@ -16,6 +22,15 @@ public class Juego extends InterfaceJuego {
         
         // Inicia el juego
         this.entorno.iniciar();
+        this.tortugas = new ArrayList<>();
+        this.random = new Random();
+
+        // Generamos un número aleatorio de tortugas
+        for (int i = 0; i < random.nextInt(3) + 2; i++) {
+            double x = random.nextDouble() * 800; // Posición aleatoria en X
+            double y = 50; // Altura inicial (cima de la pantalla)
+            tortugas.add(new Tortuga(x, y));
+        }
     }
 
     public void tick() {
@@ -39,7 +54,25 @@ public class Juego extends InterfaceJuego {
         // Disparo del corte con la barra espaciadora
         if (this.entorno.sePresiono(this.entorno.TECLA_ESPACIO)) {
             this.prota.dispararBolita();
+           
         }
+        for (Tortuga tortuga : tortugas) {
+            tortuga.mover();
+            // cambia de dirección si alcanza los bordes
+            if (tortuga.getX() <= 0 || tortuga.getX() >= 800 - 50) {
+                tortuga.cambiarDireccion();
+            }
+            // verificar si colisiona con Pep
+            if (tortuga.colisionaCon(prota)) {
+                System.out.println("¡Pep ha sido aniquilado por una tortuga!");
+                // Lógica para perder el juego o restar vidas
+            }
+        }
+
+        // Dibujar tortugas
+        for (Tortuga tortuga : tortugas) {
+            this.entorno.dibujarImagen(tortuga.getImagen(), tortuga.getX(), tortuga.getY(), 0);
+        }    
         
        
         this.entorno.dibujarImagen(
